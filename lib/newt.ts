d13a5f2448ba2605f609d42dd0241fe971d96a03
w3cdp6084-dev <yusukew3@gmail.com>
2023-08-06 14:15:35 +0900

    Newt set

---
+++ lib/newt.ts
@@ -0,0 +1,32 @@
+import { createClient } from 'newt-client-js'
+import 'server-only'
+import type { Article } from '../types/article'
+import { cache } from 'react'
+const client = createClient({
+  spaceUid: process.env.NEWT_SPACE_UID + '',
+  token: process.env.NEWT_CDN_API_TOKEN + '',
+  apiType: 'cdn',
+})
+
+
+export const getArticles = cache(async () => {
+    const { items } = await client.getContents<Article>({
+        appUid: 'blog',
+        modelUid: 'article',
+        query: {
+            select: ['_id', 'title', 'slug', 'body'],
+        },
+    })
+    return items
+})
+export const getArticleBySlug = cache(async (slug: string) => {
+    const article = await client.getFirstContent<Article>({
+      appUid: 'blog',
+      modelUid: 'article',
+      query: {
+        slug,
+        select: ['_id', 'title', 'slug', 'body'],
+      },
+    })
+    return article
+  })
