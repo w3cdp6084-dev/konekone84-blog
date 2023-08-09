688b4275ec2a85556076bd7a80f5423aa73d08fb
w3cdp6084-dev <yusukew3@gmail.com>
2023-08-09 13:43:20 +0900

    reset add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Provider from "./Provider";
 import Header from "./components/Header";
-import { Navbar } from "./components/Navbar.client";
+import "./sass/reset.scss"
 const inter = Inter({ subsets: ['latin'] })
 
 const config = {
