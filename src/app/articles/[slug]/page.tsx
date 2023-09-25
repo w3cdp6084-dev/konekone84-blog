980144524b89d7acb8d1d7ce052d97066675713e
w3cdp6084-dev <yusukew3@gmail.com>
2023-09-25 14:33:30 +0900

    目次雛形実装

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -4,6 +4,8 @@ import type { Article } from '../../../../types/article'
 import PageTransition from '../../components/PageTransition';
 import { useClientPath } from '../../hooks/useClientPath';
 import ShareButtons from '../../components/ShareButtons';
+import TableOfContents from '../../components/TableOfContents';
+import { load } from 'cheerio'
 
 type Props = {
   params: {
@@ -34,10 +36,32 @@ export default async function Article({ params }: Props) {
   const article = await getArticleBySlug(slug)
   if (!article) return
   const path = typeof window !== 'undefined' ? useClientPath() : '';
+  if (!article) return
+  const headings: string[] = []
+  const $ = load(article.body.toString()) // 1
+ $('h2').each((_, elm) => { // 2
+   const text = $(elm).text() // 3
+   headings.push(text)
+   $(elm).contents().wrap(`<a id="${text}" href="#${text}"></a>`) // 4
+ })
+ article.body = $.html() // 5
+
   return (
     <PageTransition path={path}>
     <main>
       <h1>{article.title}</h1>
+      <div>
+        <h2>Table Of Contents</h2>
+        <ol>
+          {headings.map((text, index) => {
+            return (
+              <li key={index}>
+                <a href={`#${text}`}>{text}</a>
+              </li>
+            )
+          })}
+        </ol>
+      </div>
       <h2 id="section1">{article.subtitleh2}</h2>
       <h3 id="section1-subsection1">{article.subtitleh3}</h3>
       <div dangerouslySetInnerHTML={{ __html: article.body }} />
